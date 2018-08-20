b8403c51c8ef6616e5c1f461489e9b1cd31749ec
Daniel Wang <y444444y@hotmail.com>
2018-08-20 19:30:56 +0800

    added new option to process pasted text from quizlet export (creates dict in stacks)

--- quizlet_dict.py
+++ quizlet_dict.py
@@ -1,6 +1,6 @@
 #Use this module to generate a dictionary to use in quizlet.py
 
-def dict_generator(max_words = 50, copy = False):
+def dict_generator(max_words = 50, copy = False, from_export = False):
     """Enter in key, value; repeat
     PRESS EXIT ANY TIME TO END SESSION
 
@@ -10,6 +10,8 @@ def dict_generator(max_words = 50, copy = False):
         max number of iterations. optional (default = 50)
     copy (bool):
         whether to copy stack. optional (default = False)
+    from_export (bool):
+        whether to process text from quizlet export instead of generating new dict (see directions from README)
 
     returns:
     _______
@@ -17,26 +19,27 @@ def dict_generator(max_words = 50, copy = False):
     path = input("\nWelcome to the stack dictionary generator. \nWhat would you like to name your stack?\n")
     print("\n")
     path += "" if path.endswith(".txt") else ".txt"
-    print("ENTER 'SAVE'/'EXIT' ALL CAPS ANY TIME TO SAVE & EXIT, 'ABORT' TO EXIT W/O SAVING\nEnter in the term followed by the definition.\n")
-    term = []
-    defin = []
-    for i in range(max_words):
-        if i<2:
-            td = 'term' if i%2 == 0 else "definition"
-            word = input("Enter your {}: ".format(td))
-        else:
-            word = input()
-        if word == "SAVE" or word == 'EXIT':
-            break
-        elif word == "ABORT":
-            print("Stack not saved")
-            exit(2)
-        if i%2 == 0:
-            term.append(word) 
-        else:
-            defin.append(word)
-            print("")
-    stack = dict(zip(term,defin))
+    if not from_export:
+        print("ENTER 'SAVE'/'EXIT' ALL CAPS ANY TIME TO SAVE & EXIT, 'ABORT' TO EXIT W/O SAVING\nEnter in the term followed by the definition.\n")
+        term = []
+        defin = []
+        for i in range(max_words):
+            if i<2:
+                td = 'term' if i%2 == 0 else "definition"
+                word = input("Enter your {}: ".format(td))
+            else:
+                word = input()
+            if word == "SAVE" or word == 'EXIT':
+                break
+            elif word == "ABORT":
+                print("Stack not saved")
+                exit(2)
+            if i%2 == 0:
+                term.append(word) 
+            else:
+                defin.append(word)
+                print("")
+        stack = dict(zip(term,defin))
     if copy:        #if usr chooses to copy to clipboard
         try:
             import pyperclip
@@ -45,14 +48,18 @@ def dict_generator(max_words = 50, copy = False):
         except ImportError:
             print("Stack not copied")
             pass
-    if path is not None:        #save in folder "stacks" if usr chooses so
-        from pathlib import Path
-        root = Path(".")
-        path_2_stacks = root/"stacks"/path
-        with open(path_2_stacks,"a") as dic:
+
+    from pathlib import Path        #save in folder "stacks" if usr chooses so
+    root = Path(".")
+    path_2_stacks = root/"stacks"/path
+    with open(path_2_stacks,"a") as dic:
+        if not from_export:
             dic.write(str(stack))
-        print("Stack saved!")
+        else:
+            stack = '{"'+str(input("Enter text from Quizlet export (see README for details)")[:-2])+'}'
+            dic.write(stack)
+    print("Stack saved!")
     return stack
 
 #copy and paste dictionary into quizlet.py, under variable 'stack'
-print(dict_generator(10))
+print(dict_generator(10,from_export=True))
